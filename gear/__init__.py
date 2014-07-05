b09ec28bcfcc6cafa4281bb394c9e8becc42fc37
looooo <sppedflyer@gmail.com>
2014-07-05 08:47:05 +0200

    planb

---
+++ gear/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/lib/python
+
+from gearfunc._involute_tooth import involute_rack, involute_tooth
+from gearfunc._cycloide_tooth import cycloide_tooth
+from gearfunc._bevel_tooth import bevel_tooth
+from gearfunc import CreateInvoluteRack, CreateCycloideGear, CreateInvoluteGear, CreateBevelGear
+
+
+__All__ = [
+    "CreateInvoluteRack",
+    "CreateCycloideGear",
+    "CreateInvoluteGear",
+    "CreateBevelGear",
+    "involute_rack",
+    "involute_tooth",
+    "bevel_tooth"
+]
