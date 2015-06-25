00d814e8bd0e7e068deda7ef609527b8219405c7
looooo <sppedflyer@gmail.com>
2015-06-25 11:44:11 +0200

    schas

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ from distutils.core import setup
 setup(
     name = 'freecad_gear',
     packages = ['freecad_gear'],
-    version = '0.2.2',
+    version = '0.2.3',
     description = 'Some gears for freecad',
     author = 'Lorenz L',
     author_email = 'sppedflyer@gmail.com',
     url = 'https://github.com/looooo/FCGear',
-    download_url = 'https://github.com/looooo/FCGear/tarball/0.2.1',
+    download_url = 'https://github.com/looooo/FCGear/tarball/0.2.3',
     keywords = ['gear', 'freecad'],
     classifiers = [],
 )
