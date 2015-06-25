a511a2835d365d4e9bc0f56002e0a561b2bdd703
looooo <sppedflyer@gmail.com>
2015-06-25 11:01:15 +0200

    setup version

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ from distutils.core import setup
 setup(
     name = 'freecad_gear',
     packages = ['freecad_gear'],
-    version = '0.1.0',
+    version = '0.2.1',
     description = 'Some gears for freecad',
     author = 'Lorenz L',
     author_email = 'sppedflyer@gmail.com',
     url = 'https://github.com/looooo/FCGear',
-    download_url = 'https://github.com/looooo/FCGear/tarball/0.1',
+    download_url = 'https://github.com/looooo/FCGear/tarball/0.2.1',
     keywords = ['gear', 'freecad'],
     classifiers = [],
 )
