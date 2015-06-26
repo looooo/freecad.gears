62909966f2a9b69a6adf3712fd95864205c928db
looooo <sppedflyer@gmail.com>
2015-06-26 11:14:11 +0200

    manifest

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ from distutils.core import setup
 setup(
     name = 'freecad_gear',
     packages = ['freecad_gear'],
-    version = '0.2.3',
+    version = '0.2.5',
     description = 'Some gears for freecad',
     author = 'Lorenz L',
     author_email = 'sppedflyer@gmail.com',
     url = 'https://github.com/looooo/FCGear',
-    download_url = 'https://github.com/looooo/FCGear/tarball/0.2.3',
+    download_url = 'https://github.com/looooo/FCGear/tarball/0.2.5',
     keywords = ['gear', 'freecad'],
     classifiers = [],
 )
