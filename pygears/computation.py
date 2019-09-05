e4d6955b0b1d7120414548720d952345cf26f3c6
lorenz <looooo@users.noreply.github.com>
2019-09-05 08:06:10 +0200

    Add helical angle to distance calculator

--- pygears/computation.py
+++ pygears/computation.py
@@ -1,12 +1,13 @@
 import numpy as np
 from scipy import optimize as opt
 
-def computeShiftedGears(m, alpha, t1, t2, x1, x2):
+def computeShiftedGears(m, alpha, beta, t1, t2, x1=0, x2=0):
     """Summary
     
     Args:
         m (float): common module of both gears [length]
         alpha (float): pressure-angle [rad]
+        beta (float): helical angle [rad]
         t1 (int): number of teeth of gear1
         t2 (int): number of teeth of gear2
         x1 (float): relative profile-shift of gear1
@@ -15,6 +16,7 @@ def computeShiftedGears(m, alpha, t1, t2, x1, x2):
     Returns:
         (float, float): distance between gears [length], pressure angle of the assembly [rad]
     """
+    m_t = m / no.cos(beta)
     inv = lambda x: np.tan(x) - x
     inv_alpha_w = inv(alpha) + 2 * np.tan(alpha) * (x1 + x2) / (t1 + t2)
     root_inv = lambda x: inv(x) - inv_alpha_w
