8e1c02837479070f126fbd594f48df81341b5631
FC-FBXL5 <103560699+FC-FBXL5@users.noreply.github.com>
2026-04-23 13:23:04 +0200

    Extended for task panel, involutegearrack.py
    Individual view provider and task panel settings added

--- freecad/gears/involutegearrack.py
+++ freecad/gears/involutegearrack.py
@@ -17,12 +17,22 @@
 # ***************************************************************************
 
 import numpy as np
+import os
 
 from freecad import app
+from freecad import gui
 from freecad import part
 
 from pygears.involute_tooth import InvoluteRack
-from .basegear import BaseGear, fcvec, points_to_wire, insert_fillet
+from .basegear import (
+    BaseGear,
+    fcvec,
+    points_to_wire,
+    insert_fillet,
+    updateTaskTitleIcon,
+    GearsBaseTaskPanel,
+    ViewProviderGear,
+)
 
 QT_TRANSLATE_NOOP = app.Qt.QT_TRANSLATE_NOOP
 
@@ -92,6 +102,10 @@ class InvoluteGearRack(BaseGear):
         obj.simplified = False
         self.obj = obj
         obj.Proxy = self
+
+        panel = InvoluteGearRackTaskPanel(obj)
+        updateTaskTitleIcon(panel)
+        gui.Control.showDialog(panel)
 
     def onDocumentRestored(self, obj):
         """  
@@ -305,3 +319,99 @@ class InvoluteGearRack(BaseGear):
                 fcvec([0.0, np.tan(obj.rack.beta) * obj.height.Value, obj.height.Value])
             )
             return part.makeLoft([pol, pol2], True)
+
+if app.GuiUp:
+
+    class InvoluteGearRackViewProvider(ViewProviderGear):
+
+        def __init__(self, obj, icon_fn=None):
+            # Set this object to the proxy object of the actual view provider
+            obj.Proxy = self
+            self._check_attr()
+            dirname = os.path.dirname(__file__)
+            self.icon_fn = icon_fn or os.path.join(dirname, "icons", "involuterack.svg")
+
+        def _check_attr(self):
+            """
+            Check for missing attributes.
+            """
+            if not hasattr(self, "icon_fn"):
+                setattr(
+                    self,
+                    "icon_fn",
+                    os.path.join(os.path.dirname(__file__), "icons", "involuterack.svg"),
+                )
+                
+        def getTaskPanel(self, obj):
+            return InvoluteGearRackTaskPanel(obj)
+
+    class InvoluteGearRackTaskPanel(GearsBaseTaskPanel):
+        """Control panel for Involute Gears"""
+
+        def __init__(self, obj):
+
+            self.obj = obj
+
+            self.num_teeth = obj.num_teeth
+            self.module = obj.module
+            self.height = obj.height
+            self.thickness = obj.thickness
+            self.helix_angle = obj.helix_angle
+            self.double_helix = obj.double_helix
+
+            self.transverse_pitch = obj.transverse_pitch.Value  # uses existing logic
+            self.auto_recompute = False
+
+            self.translateTaskPanel()
+
+            #- Add a QGroupBox container with a QGridLayout to group widgets
+            self.group_box_0, self.grid_0 = self.groupBoxWithGrid()
+            self.group_box_0.setWindowTitle(QT_TRANSLATE_NOOP(
+                "Gear_TaskPanel",
+                "Involute Gear Rack Parameters"
+            ))
+            #- Add sub-boxes
+            self.group_box_1, self.grid_1 = self.groupBoxWithGrid(
+                QT_TRANSLATE_NOOP("Gear_TaskPanel", "Size")
+            )
+            self.group_box_1.setStyleSheet("background-color:#dec")
+            self.grid_0.addWidget(self.group_box_1, 0, 0)
+
+            self.group_box_2, self.grid_2 = self.groupBoxWithGrid(
+                QT_TRANSLATE_NOOP("Gear_TaskPanel", "Helix Angle")
+            )
+            self.group_box_2.setStyleSheet("background-color:#def")
+            self.grid_0.addWidget(self.group_box_2, 1, 0)
+
+            self.group_box_3, self.grid_3 = self.groupBoxWithGrid(
+                QT_TRANSLATE_NOOP("Gear_TaskPanel", "Gear Width")
+            )
+            self.group_box_3.setStyleSheet("background-color:#fed")
+            self.grid_0.addWidget(self.group_box_3, 2, 0)
+
+            self.group_box_4, self.grid_4 = self.groupBoxWithGrid(
+                QT_TRANSLATE_NOOP("Gear_TaskPanel", "Thickness")
+            )
+            self.group_box_4.setStyleSheet("background-color:#ddd")
+            self.grid_0.addWidget(self.group_box_4, 3, 0)
+
+            self.group_box_5, self.grid_5 = self.groupBoxWithGrid(
+                QT_TRANSLATE_NOOP("Gear_TaskPanel", "Update")
+            )
+            self.group_box_5.setStyleSheet("background-color:#ddd")
+            self.grid_0.addWidget(self.group_box_5, 10, 0)
+
+            #- Add some widgets to the grids
+            self.addSizeWidgetsInvoluteRack(self.grid_1)
+
+            self.addHelixAngleWidgets(self.grid_2)
+
+            self.addTransverseHightWidgets(self.grid_3)
+
+            self.addThicknessWidgets(self.grid_4)
+
+            self.addUpdateWidgets(self.grid_5)
+
+            self.form = self.group_box_0
+            return
+
