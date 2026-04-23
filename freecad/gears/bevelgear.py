84696fadf9c0194cf4c009084ff6aecc5efe281c
FC-FBXL5 <103560699+FC-FBXL5@users.noreply.github.com>
2026-04-23 14:08:47 +0200

    Extended for task panel, bevelgear.py
    Individual view provider and task panel settings added

--- freecad/gears/bevelgear.py
+++ freecad/gears/bevelgear.py
@@ -16,15 +16,24 @@
 # *                                                                         *
 # ***************************************************************************
 
-from freecad import app
-from freecad import part
-
 import numpy as np
+import os
+
+from freecad import app
+from freecad import gui
+from freecad import part
 
 from pygears.bevel_tooth import BevelTooth
 from pygears._functions import rotation3D
 
-from .basegear import BaseGear, fcvec, make_bspline_wire
+from .basegear import (
+    BaseGear,
+    fcvec,
+    make_bspline_wire,
+    updateTaskTitleIcon,
+    GearsBaseTaskPanel,
+    ViewProviderGear,
+)
 
 QT_TRANSLATE_NOOP = app.Qt.QT_TRANSLATE_NOOP
 
@@ -152,6 +161,10 @@ class BevelGear(BaseGear):
         self.obj = obj
         obj.Proxy = self
 
+        panel = BevelGearTaskPanel(obj)
+        updateTaskTitleIcon(panel)
+        gui.Control.showDialog(panel)
+
     def generate_gear_shape(self, fp):
         fp.gear.z = fp.num_teeth
         fp.gear.module = fp.module.Value
@@ -257,3 +270,89 @@ class BevelGear(BaseGear):
     def spherical_rot(self, point, phi):
         new_phi = np.sqrt(np.linalg.norm(point)) * phi
         return rotation3D(-new_phi)(point)
+
+if app.GuiUp:
+
+    class BevelGearViewProvider(ViewProviderGear):
+
+        def __init__(self, obj, icon_fn=None):
+            # Set this object to the proxy object of the actual view provider
+            obj.Proxy = self
+            self._check_attr()
+            dirname = os.path.dirname(__file__)
+            self.icon_fn = icon_fn or os.path.join(dirname, "icons", "bevelgear.svg")
+
+        def _check_attr(self):
+            """
+            Check for missing attributes.
+            """
+            if not hasattr(self, "icon_fn"):
+                setattr(
+                    self,
+                    "icon_fn",
+                    os.path.join(os.path.dirname(__file__), "icons", "bevelgear.svg"),
+                )
+
+        def getTaskPanel(self, obj):
+            return BevelGearTaskPanel(obj)
+
+    class BevelGearTaskPanel(GearsBaseTaskPanel):
+        """Control panel for Involute Gears"""
+
+        def __init__(self, obj):
+
+            self.obj = obj
+            #- Input values
+            self.module = obj.module
+            self.num_teeth = obj.num_teeth
+            self.height = obj.height
+            self.beta = obj.beta
+            #- Default values for computed properties
+            self.pitch_diameter = obj.dw.Value
+            self.auto_recompute = False
+
+            self.translateTaskPanel()
+
+            #- Add a QGroupBox container with a QGridLayout to group widgets
+            self.group_box_0, self.grid_0 = self.groupBoxWithGrid()
+            self.group_box_0.setWindowTitle(QT_TRANSLATE_NOOP(
+                "Gear_TaskPanel",
+                "Bevel Gear Parameters"
+            ))
+            #- Add sub-boxes
+            self.group_box_1, self.grid_1 = self.groupBoxWithGrid(
+                QT_TRANSLATE_NOOP("Gear_TaskPanel", "Size")
+            )
+            self.group_box_1.setStyleSheet("background-color:#dec")
+            self.grid_0.addWidget(self.group_box_1, 0, 0)
+
+            self.group_box_2, self.grid_2 = self.groupBoxWithGrid(
+                QT_TRANSLATE_NOOP("Gear_TaskPanel", "Spiral Angle")
+            )
+            self.group_box_2.setStyleSheet("background-color:#def")
+            self.grid_0.addWidget(self.group_box_2, 1, 0)
+
+            self.group_box_3, self.grid_3 = self.groupBoxWithGrid(
+                QT_TRANSLATE_NOOP("Gear_TaskPanel", "Gear Width")
+            )
+            self.group_box_3.setStyleSheet("background-color:#fed")
+            self.grid_0.addWidget(self.group_box_3, 2, 0)
+
+            self.group_box_4, self.grid_4 = self.groupBoxWithGrid(
+                QT_TRANSLATE_NOOP("Gear_TaskPanel", "Update")
+            )
+            self.group_box_4.setStyleSheet("background-color:#ddd")
+            self.grid_0.addWidget(self.group_box_4, 10, 0)
+
+            #- Add some widgets to the grids
+            self.addSizeWidgetsBevel(self.grid_1)
+
+            self.addBetaAngleWidgets(self.grid_2)
+
+            self.addTransverseHightWidgets(self.grid_3)
+
+            self.addUpdateBevelWidgets(self.grid_4)
+
+            self.form = self.group_box_0
+            return
+
